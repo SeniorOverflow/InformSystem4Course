ca259f80c0b218200bab50219bcf9e437bd9b30a
igor.popov <senover@DESKTOP-VMI6T41>
2024-10-06 17:06:10 +0500

    Переработка базы под тек задачи

---
+++ Lab3Core/include/Lab3Core.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <vector>
+#include <array>
+
+
+
+
+class Lab3Core final
+{
+
+private:
+
+
+public:
+    Lab3Core();
+    ~Lab3Core();
+
+private:
+
+};
---
+++ Lab5Core/src/Lab5Core.cpp
@@ -0,0 +1,12 @@
+#include "Lab5Core.h"
+
+
+Lab5Core::Lab5Core()
+{
+
+}
+
+Lab5Core::~Lab5Core()
+{
+
+}
